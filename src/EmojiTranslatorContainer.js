822f5ced5b62b84aab5bd5d05c9fce1a3d17287c
github username <github email address>
2017-05-26 15:44:21 -0400

    persisting story into db

--- src/EmojiTranslatorContainer.js
+++ src/EmojiTranslatorContainer.js
@@ -5,14 +5,68 @@ import Stories from './components/Stories';
 export default class EmojiTranslatorContainer extends Component {
   constructor() {
     super()
+    this.state = {
+      originalContent: '',
+      creator: '',
+      translatedContent: '',
+      stories: []
+    }
+  }
+
+  handleTranslate(event) {
+    this.setState({
+      originalContent: event.target.value,
+      translatedContent: event.target.value.toUpperCase()
+    })
+    console.log('from handleTranslate: ', this.state.originalContent);
+  }
+
+  handleCreator(event) {
+    this.setState({
+       creator: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    fetch('http://localhost:3000/stories', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({story: {
+        original_content: this.state.originalContent,
+        creator: this.state.creator,
+        translated_content: this.state.translatedContent
+      }})
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
 
   }
 
-  render(){
+  // translate() {
+  //   this.setState({
+  //     translatedContent:
+  //   })
+  // }
+
+
+
+  render() {
     return(
       <div>
-        <Translator />
-        <Stories />
+        <Translator
+          handleTranslate={this.handleTranslate.bind(this)}
+          translatedContent={this.state.translatedContent}
+          handleSubmit={this.handleSubmit.bind(this)}
+          handleCreator={this.handleCreator.bind(this)}
+        />
+        <Stories
+        originalContent={this.state.originalContent}
+        translatedContent={this.state.translatedContent}
+      />
       </div>
     )
   }
--- src/components/Translator.js
+++ src/components/Translator.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
-export default () => {
+export default (props) => {
   return(
-    <h1>This is a Translator component</h1>
+    <div>
+      <h1>This is a Translator component</h1>
+      <form onSubmit={props.handleSubmit}>
+        <textarea id="original" onChange={props.handleTranslate} placeholder="Enter your story here:"/>
+        <br></br>
+        <input
+          onChange={props.handleCreator}
+          id="creator"
+          type="text"
+          placeholder="Author"
+        />
+        <br></br>
+
+        <textarea
+          id="translated"
+          value={props.translatedContent}
+        />
+          <br></br>
+
+        <input type="submit" value="Save Story"/>
+
+      </form>
+    </div>
   )
 }
