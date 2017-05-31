3d34509346d266f19406a5df741b4fb6bd3a72fd
meryldakin <meryl.k.dakin@gmail.com>
2017-05-31 15:54:46 -0400

    plaing with parallax

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export default (props) => {
+
+
   return(
     <div className="container black">
       <h1 className="center-align">Stories</h1>
@@ -9,12 +11,13 @@ export default (props) => {
           <div className="container">
             <div className="card">
               <div className="card-content">
-                <span className="card-title ">{story.creator}</span>
-                <p className="">{story.translated_content}</p>
+                <span className="card-title ">{story.title}</span>
+                <p id="translated-text">{story.translated_content}</p>
               </div>
               <div className="card-action">
                 <a href="#translatorBox" onClick={function(){props.edit(story.id)}}> Edit </a>
                 <a href="#" onClick={function(){props.delete(story.id)}}> Delete </a>
+
               </div>
             </div>
 
@@ -22,6 +25,7 @@ export default (props) => {
 
         )
       }).reverse()}
+      
     </div>
   )
 }
